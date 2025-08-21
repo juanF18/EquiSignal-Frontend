8263acd009e5c152a43d8a24ac49b0519a71e713
Juan Felipe <juanfelipecortescastrillon@gmail.com>
2025-08-20 23:10:42 -0500

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button"; // así lo trae shadcn-vue
+
+function handleClick() {
+  alert("¡Hola desde shadcn-vue! 🚀");
+}
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <Button @click="handleClick"> Click aquí ✨ </Button>
+  </div>
+</template>
