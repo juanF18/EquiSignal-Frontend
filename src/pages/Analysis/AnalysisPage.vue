f773c6335f820b4e1529f9115e0410ab7286dd24
Juan Felipe Cortes Castrillon <juanfelipecortescastrillon@gmail.com>
2025-08-24 22:32:32 -0500

    feat: add a router in header buttons

---
+++ src/pages/Analysis/AnalysisPage.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+// aquí en el futuro agregaremos el contenido real
+</script>
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold text-gray-800">This is the Analysis page</h1>
+  </div>
+</template>
