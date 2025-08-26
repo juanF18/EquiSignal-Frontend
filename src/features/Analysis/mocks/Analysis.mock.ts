1ea45f25a48f26986128fe0e8663e0a4969304c7
Juan Felipe Cortes Castrillon <juanfelipecortescastrillon@gmail.com>
2025-08-25 22:24:46 -0500

    feat: add tabs to navigation and firs chart

---
+++ src/features/Analysis/mocks/Analysis.mock.ts
@@ -0,0 +1,34 @@
+export const mockData = {
+    data: [
+        {
+            Ticker: "SSRM",
+            Company: "Silver Standard Resources",
+            TargetFrom: "$13.85",
+            TargetTo: "$18.95",
+        },
+        {
+            Ticker: "MEG",
+            Company: "Montrose Environmental Group",
+            TargetFrom: "$30.00",
+            TargetTo: "$31.00",
+        },
+        {
+            Ticker: "STT",
+            Company: "State Street",
+            TargetFrom: "$116.00",
+            TargetTo: "$125.00",
+        },
+        {
+            Ticker: "SXI",
+            Company: "Standex International",
+            TargetFrom: "$175.00",
+            TargetTo: "$205.00",
+        },
+        {
+            Ticker: "IMAX",
+            Company: "IMAX",
+            TargetFrom: "$30.00",
+            TargetTo: "$32.00",
+        },
+    ],
+};
